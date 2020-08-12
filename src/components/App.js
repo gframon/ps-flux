4682eb58042a98b289ea3b442f2ff2f2d7104656
Ramon Gomez <ramon.gomez@noaa.gov>
2020-08-12 12:53:02 -0400

    Created TextInput and CourseForm component. Implemented form submit and change events. Implemented Link functionality. Added course route.

--- src/components/App.js
+++ src/components/App.js
@@ -16,7 +16,8 @@ function App() {
                 <Route path="/" exact component={HomePage} />
                 <Route path="/courses" component={CoursesPage} />
                 <Route path="/about" component={AboutPage} />
-                <Route path="/course/:slug" component={ManageCoursePage} /> 
+                <Route path="/course/:slug" component={ManageCoursePage} />
+                <Route path="/course/" component={ManageCoursePage} /> 
                 <Redirect from="/about-page" to="/about" />
                 <Route component={NotFoundPage} />
             </Switch>
--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import CourseForm from './CourseForm';
+import * as courseApi from '../api/courseApi';
 
 const ManageCoursePage = props => {
+    const [ course, setCourse ] = useState({
+        id: null,
+        slug: "",
+        title: "",
+        authorId: null,
+        category: ""
+    });
+
+    function handleChange({ target }) {
+        setCourse({ 
+            ...course, 
+            [target.name]: target.value 
+        });
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        courseApi.saveCourse(course);
+    }
     return (
         <>
             <h2>Manage Course</h2>
-            {props.match.params.slug}
+            <CourseForm course={course} onChange={handleChange} onSubmit={handleSubmit}/>
         </>
     );
 }
