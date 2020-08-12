70d66fece5dee148ccc0ca171ebb5a6b6739c115
Ramon Gomez <ramon.gomez@noaa.gov>
2020-08-12 09:50:34 -0400

    Added Redirect and Link functionality.  Created ManageCoursePage.js.

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,9 @@ import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 import CoursesPage from './CoursesPage';
 import Header from './common/Header';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import NotFoundPage from './NotFoundPage';
+import ManageCoursePage from './ManageCoursePage';
 
 function App() {
     
@@ -13,8 +14,10 @@ function App() {
             <Header />
             <Switch>
                 <Route path="/" exact component={HomePage} />
-                <Route path="/courses" exact component={CoursesPage} />
-                <Route path="/about" exact component={AboutPage} />
+                <Route path="/courses" component={CoursesPage} />
+                <Route path="/about" component={AboutPage} />
+                <Route path="/course/:slug" component={ManageCoursePage} /> 
+                <Redirect from="/about-page" to="/about" />
                 <Route component={NotFoundPage} />
             </Switch>
         </div>
