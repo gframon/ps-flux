4bce92b20073507164712bce7604e509d24a30fd
Ramon Gomez <ramon.gomez@noaa.gov>
2020-08-13 14:36:34 -0400

    Implemented Flux.

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import CourseForm from "./CourseForm";
-import * as courseApi from "../api/courseApi";
+import courseStore from "../stores/courseStore";
 import { toast } from "react-toastify";
+import * as courseActions from "../actions/courseActions";
 
 const ManageCoursePage = (props) => {
   const [errors, setErrors] = useState({});
@@ -15,8 +16,7 @@ const ManageCoursePage = (props) => {
 
   useEffect(() => {
     const slug = props.match.params.slug; // rom the path `/courses/:slug`
-    if (slug)
-      courseApi.getCourseBySlug(slug).then((_course) => setCourse(_course));
+    if (slug) setCourse(courseStore.getCourseBySlug(slug));
   }, [props.match.params.slug]);
 
   function handleChange({ target }) {
@@ -41,8 +41,7 @@ const ManageCoursePage = (props) => {
   function handleSubmit(event) {
     event.preventDefault();
     if (!formIsValid()) return;
-
-    courseApi.saveCourse(course).then(() => {
+    courseActions.saveCourse(course).then(() => {
       props.history.push("/courses");
       toast.success("Course saved.");
     });
